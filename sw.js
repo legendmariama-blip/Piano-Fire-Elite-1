eaa67f1021a6282d584a6f5891c71007205e3bc8
legendmariama-blip <legendmariama@gmail.com>
2026-04-15 13:52:29 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const cacheName = 'piano-fire-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
